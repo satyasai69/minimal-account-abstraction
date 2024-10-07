eb3745d17132c90131d1f47c41a636049be029c1
satyasai69 <ch.satya.sai133@gmail.com>
2024-10-07 15:30:37 +0530

    validateUserOp

--- src/ethereum/MinimalAccount.sol
+++ src/ethereum/MinimalAccount.sol
@@ -5,6 +5,8 @@ import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.so
 import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {SIG_VALIDATION_SUCCESS, SIG_VALIDATION_FAILED} from "account-abstraction/core/Helpers.sol";
 
 contract MinimalAccount is IAccount, Ownable {
     constructor() Ownable(msg.sender) {}
@@ -13,11 +15,27 @@ contract MinimalAccount is IAccount, Ownable {
         external
         returns (uint256 validationData)
     {
-        validationData = _validateUserOp(userOp, userOpHash, missingAccountFunds);
+        validationData = _validateUserOp(userOp, userOpHash);
+        _payPreFunds(missingAccountFunds);
     }
 
-    function _validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
+    function _validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash)
         internal
+        view
         returns (uint256 validationData)
-    {}
+    {
+        bytes32 ethSignedMessageHash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
+        address signer = ECDSA.recover(ethSignedMessageHash, userOp.signature);
+        if (signer != owner()) {
+            return SIG_VALIDATION_FAILED;
+        }
+        return SIG_VALIDATION_SUCCESS;
+    }
+
+    function _payPreFunds(uint256 missingAccountFunds) internal {
+        if (missingAccountFunds > 0) {
+            (bool success,) = payable(msg.sender).call{value: missingAccountFunds, gas: type(uint256).max}("");
+            (success);
+        }
+    }
 }
